6282d8795c9523f08e61728a15cc254d1d43e3c2
danielkansaon <danielkansaon>
2018-11-01 19:43:31 -0300

    1088

--- 1088-bolhasbaldes.cpp
+++ 1088-bolhasbaldes.cpp
@@ -1,10 +1,61 @@
 //Utilizar o merge-sorte nlogn. Paradigma: dividir para conquistar
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+void merge(int *v1 int inicio, int meio, int fim, int *qtd_trocas)
+{
+    int vetor_aux [fim + 1] = {};
+    int pos_v1 = inicio, pos_v2 = meio, pos_aux = 0;
+
+    cout << v1[0];
+    while (pos_aux <= fim)
+    {
+        if (pos_v1 <= meio && v1[pos_v1] < v1[pos_v2])
+        {
+            vetor_aux[pos_aux] = v1[pos_v1];
+            pos_v1++;
+        }
+        else
+        {
+            vetor_aux[pos_aux] = v1[pos_v2];
+            pos_v2++;
+
+            *qtd_trocas += (meio - pos_v1);
+        }
+
+        pos_aux++;
+    }
+
+    for(i = inicio; i <= posicaoFim; i++) {
+        v1[i] = vetor_aux[i - inicio];
+    }
+}
+
+void merge_sort(int v*, int inicio, int fim, int *qtd_trocas)
+{
+    int meio = 0;
+    if ((fim == inicio)) // Caso base (1 posicoes)
+    {
+        //return 0;
+    }
+    else if (fim - inicio == 1) // Caso base (2 posicoes)
+    {
+        merge(v, 0, 0, 1, qtd_trocas);
+    }
+    else
+    {
+        meio = (inicio + fim) / 2;
+        merge_sort(v, inicio, meio, qtd_trocas);
+        merge_sort(v, meio + 1, fim, qtd_trocas);        
+        merge(v, inicio, meio, fim, qtd_trocas);
+    }
+}
 
 int main()
 {
-    int qtd, qtd_trocas;
+    int qtd = 0;
+    int trocas = 0;
 
     cin >> qtd;
 
@@ -14,57 +65,9 @@ int main()
         cin >> numbers[i];
     }
 
-    merge_sort(numbers, 0, qtd - 1, &qtd_trocas);
-    cout << "teste";
-}
+    merge_sort(numbers, 0, (qtd - 1) , &trocas);
 
-int *merge(int v1[], int v2[], int meio, int fim, int *qtd_trocas)
-{
-    int vetor_aux[fim + 1];
-    int pos_v1 = 0, pos_v2 = 0, pos_aux = 0;
+    cout << trocas;
 
-    while (pos_aux <= fim)
-    {
-        if (pos_v1 <= meio && v1[pos_v1] < v2[pos_v2])
-        {
-            vetor_aux[pos_aux] = v1[pos_v1];
-            pos_v1++;
-        }
-        else
-        {
-            vetor_aux[pos_aux] = v2[pos_v2];
-            pos_v2++;
-
-            *qtd_trocas += (meio - pos_v1);
-        }
-
-        pos_aux++;
-    }
-
-    return vetor_aux;
-}
-
-int *merge_sort(int v[], int inicio, int fim, int *qtd_trocas)
-{
-    int meio;
-
-    if ((fim - inicio) == 1)
-    {
-        return v;
-    }
-    else if (fim - inicio == 2)
-    {
-        int v1[1] = {v[0]};
-        int v2[1] = {v[1]};
-        return merge(v1, v2, 1, 2, qtd_trocas);
-    }
-    else
-    {
-        meio = (inicio + fim) / 2;
-        int *v1 = merge_sort(v, inicio, meio, qtd_trocas);
-        int *v2 = merge_sort(v, meio + 1, fim, qtd_trocas);
-        return merge(v1, v2, meio, fim, qtd_trocas);
-    }
-
-    return v;
+    system("PAUSE");
 }
