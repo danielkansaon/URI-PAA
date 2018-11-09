5b9eb0ba068177f6aeeb9e34c59a5e06cf45d3ff
danielkansaon <danielkansaon>
2018-11-09 12:50:51 -0200

    1642 - Feito

--- 1642-tecladoquebrado.cpp
+++ 1642-tecladoquebrado.cpp
@@ -1,5 +1,58 @@
+//O algoritmo trata da resolução do problema 1642 - Teclado Quebrado do URI.
+//Para a resolução foi utilizado a ideia de subsequência, aumentando conforme possível.
+#include <iostream>
 
+using namespace std;
 
-int main(){
-    
+char vetor_texto_input[1000001];
+int vetor_qtd_caractere[128]; //Array que armazena a ocorrência de um caractere em uma subsequência.
+int qtd_teclas_funcionando = 0;
+
+int retornar_maior_sequencia() {
+	int qtd_letras_distintas = 0, maior_sequencia = 0, i = 0, j = 0;
+	
+	//Se j estiver na última posição do vetor, não há como encontrar uma sequência maior do que a atual.
+	for (i = 0; vetor_texto_input[j] != '\0'; i++)
+	{
+		while ((qtd_teclas_funcionando - qtd_letras_distintas) >= 0)
+		{
+			if (vetor_texto_input[j] == '\0') break;
+
+			//Verifica se o próximo caractere já está na sequência analisada (i até j)
+			if (vetor_qtd_caractere[int(vetor_texto_input[j])] == 0) {
+				if (qtd_teclas_funcionando - (qtd_letras_distintas + 1) >= 0) qtd_letras_distintas++;
+				else break;
+			}
+
+			vetor_qtd_caractere[int(vetor_texto_input[j])] += 1;
+			j++;
+		}
+
+		maior_sequencia = (j - i) > maior_sequencia ? (j - i) : maior_sequencia;
+
+		if (vetor_qtd_caractere[int(vetor_texto_input[i])] == 1)
+			qtd_letras_distintas += -1;
+		
+		vetor_qtd_caractere[int(vetor_texto_input[i])] += -1;
+	}
+
+	return maior_sequencia;
+}
+
+int main()
+{
+	cin >> qtd_teclas_funcionando;
+	
+	while (qtd_teclas_funcionando != 0)
+	{
+		cin.ignore(128, '\n');
+		cin.getline(vetor_texto_input, sizeof(vetor_texto_input));
+		cout << retornar_maior_sequencia() << endl;
+
+		cin >> qtd_teclas_funcionando;
+		fill_n(vetor_texto_input, 1000001, 0);
+		fill_n(vetor_qtd_caractere, 128, 0);
+	}
+
+	return 0;
 }
